f61395596dd5c8b0381b5ef143365866c55517ae
Max Bakulin <mbakulin102@gmail.com>
2022-12-02 08:40:20 +0300

    HM_2_Task_2

---
+++ HomeWork_2/Task_2/Task_2.py
@@ -0,0 +1,25 @@
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Число должно быть integer ")
+    return number
+
+
+def mult(n):
+    if n == 1:
+        return 1
+    else:
+        return n * mult(n - 1)
+
+
+num = InputNumbers("Введите число: ")
+
+list = []
+for e in range(1, num + 1):
+    list.append(mult(e))
+
+print(f"Произведения чисел от 1 до {num}:  {list}")
