c657cfb2423001d86eccee36d4bd3b5f0a039af9
Max Bakulin <mbakulin102@gmail.com>
2022-12-21 17:31:53 +0300

    HM_7_Task_1_file_2

---
+++ HomeWork_7/Task_1/Calculator/calculation_logic.py
@@ -0,0 +1,12 @@
+def Sum(a, b):
+    c = a + b
+    return c
+def Subtraction(a, b):
+    c = a - b
+    return c
+def Division(a, b):
+    c = a / b
+    return c
+def Multiplication(a, b):
+    c = a * b
+    return c
